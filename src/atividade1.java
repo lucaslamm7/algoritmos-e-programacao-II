bf26bd5d22052412d724cd0a1d247f9e3df101eb
lucaslamm7 <lucas.lammel.ifrs@gmail.com>
2021-06-06 19:56:34 -0300

    1 2 3

---
+++ src/atividade1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class atividade1 {
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+
+        int x[] = new int[20], a = 0;
+
+        System.out.println("Escreva 20 numeros inteiros: ");
+
+        for(int i = 0; i < 20; i++){
+            x[i] = ler.nextInt();
+            if((x[i]%2) == 0){
+                a++;
+            }
+        }
+        System.out.println("Existem "+ a +" valores pares");
+    }
+
+}
---
+++ src/atividade2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class atividade2 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int a[] = new int[10], b[] = new int [10], c[] = new int[10];
+
+        System.out.println("Digite 10 numeros: ");
+        for(int i=0; i<10; i++){
+            a[i] = ler.nextInt();
+        }
+        System.out.println("Digite mais 10 numeros: ");
+        for(int i=0; i<10; i++){
+            b[i] = ler.nextInt();
+        }
+        for(int i=0; i<10; i++){
+            c[i] = (a[i]*b[i]);
+        }
+        System.out.println("O vetor multiplicacao eh: ");
+        for(int i=0; i<10; i++){
+            System.out.println("A multiplicacao do indice "+ (i+1) +" eh: "+ c[i]);
+        }
+    }
+}
