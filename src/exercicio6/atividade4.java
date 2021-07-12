08202f5a7de2eafeb5a1a6ea350742d32c747685
lucaslamm7 <lucas.lammel.ifrs@gmail.com>
2021-07-12 19:40:44 -0300

    exercicio 6

--- src/exercicio6/atividade4.java
+++ src/exercicio6/atividade4.java
@@ -4,4 +4,22 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class atividade4 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        String nome;
+        ArrayList<String> nomes = new ArrayList();
+
+        nomes.add("Lionel Messi");
+        nomes.add("Cristiano Ronaldo");
+        nomes.add("Robert Lewandowski");
+        nomes.add("Kevin De Bruyne");
+        nomes.add("Neymar Jr");
+        nomes.add("Jan Oblak");
+        nomes.add("Virgil van Dijk");
+        nomes.add("Mohamed Salah");
+        nomes.add("Sadio Mané");
+        nomes.add("Alisson Becker");
+
+
+    }
 }
